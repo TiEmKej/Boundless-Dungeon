dbfb1df3b19c2ee22884a52c8e469af9506b359f
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2023-07-17 20:14:13 +0200

    Random Seed Generator and setter

---
+++ Assets/GameSeedController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSeedController : MonoBehaviour
+{
+    public int seed = 0;
+
+    private void Start()
+    {
+        if (seed == 0)
+        {
+            seed = Random.Range(0, 1000);
+        }
+        UnityEngine.Random.InitState(seed);
+    }
+
+    public int GetGameSeed()
+    {
+        return seed;
+    }
+}
