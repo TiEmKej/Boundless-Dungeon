d84bd2e48c70f27e6a0a15621c9db23c4f5979d6
Miłosz Andrzej Piekarski <milosz.piekarski2003@gmail.com>
2023-07-18 23:03:38 +0200

    Random room generator without walls and corridors

--- Assets/TileMapgeneratorTest.cs
+++ Assets/TileMapgeneratorTest.cs
@@ -10,15 +10,15 @@ public class TileMapgeneratorTest : MonoBehaviour
     public int minRoomSize = 3;
     public int maxRooms = 10;
     public int minRooms = 5;
+    public int minDistanceToRoom = 5;
+    public int mapSize = 50;
     public TileBase floorTile;
     public Tilemap floorMap;
     public Vector2Int[,] roomsList;
-    public int 
 
     // Start is called before the first frame update
     void Start()
     {
-
         PickRoomPosition(); 
     }
 
@@ -35,19 +35,19 @@ public class TileMapgeneratorTest : MonoBehaviour
         //Room dimensions
         Vector2Int roomDimension = new Vector2Int(5, 5);
         //Bool for overlap
-        bool isOverlaping = false;
+        bool isOverlaping = true;
         //While script isn't ganerate all rooms...
         while (roomsCounter <= roomsNumber)
         {
             //If it's not first room
             if(roomsCounter != 0)
             {
+                isOverlaping = true;
                 //Repeat until all tiles where room should be are free
-                while (!isOverlaping)
+                while (isOverlaping)
                 {
                     //Pick random position on the map
-                    roomCenterLocation = new Vector2Int(UnityEngine.Random.Range(-40, 40), UnityEngine.Random.Range(-40, 40));
-                    Debug.Log("Location Picked");
+                    roomCenterLocation = new Vector2Int(UnityEngine.Random.Range(-mapSize, mapSize), UnityEngine.Random.Range(-mapSize, mapSize));
                     //Save room dimension
                     roomDimension = new Vector2Int(UnityEngine.Random.Range(minRoomSize, maxRoomSize + 1), UnityEngine.Random.Range(minRoomSize, maxRoomSize + 1));
                     //Check if room is overlaping
@@ -68,13 +68,14 @@ public class TileMapgeneratorTest : MonoBehaviour
     private bool CheckRoomOverlap(Vector2Int roomCentralLocation, Vector2Int roomDimension)
     {
         Debug.Log("Checking overlap");
-        for (int i = roomCentralLocation.x - ((roomDimension.x + 2) / 2); i < roomCentralLocation.x + ((roomDimension.x + 2) / 2); i++)
+        for (int i = roomCentralLocation.x - ((roomDimension.x + minDistanceToRoom*2) / 2); i < roomCentralLocation.x + ((roomDimension.x + minDistanceToRoom * 2) / 2); i++)
         {
-            for (int j = roomCentralLocation.y - ((roomDimension.y + 2) / 2); j < roomCentralLocation.y + ((roomDimension.y + 2) / 2); j++)
+            for (int j = roomCentralLocation.y - ((roomDimension.y + minDistanceToRoom * 2) / 2); j < roomCentralLocation.y + ((roomDimension.y + minDistanceToRoom * 2) / 2); j++)
             {
 
                 if (floorMap.HasTile(new Vector3Int(i,j,0)))
                 {
+                    Debug.Log("OVERLAP");
                     return true;
                 }
             }
@@ -85,14 +86,11 @@ public class TileMapgeneratorTest : MonoBehaviour
     private void GenerateRoom(Vector2Int roomCentralLocation, Vector2Int roomDimension)
     {
         Debug.Log("Generating room");
-        Debug.Log(roomCentralLocation);
-        Debug.Log(roomDimension);
         for (int i = roomCentralLocation.x - (roomDimension.x / 2); i < roomCentralLocation.x + (roomDimension.x / 2); i++)
         {
             for (int j = roomCentralLocation.y - (roomDimension.y / 2); j < roomCentralLocation.y + (roomDimension.y / 2); j++)
             {    
                 Vector3Int tilePosition = new Vector3Int(i, j, 0);
-                Debug.Log(tilePosition);
                 floorMap.SetTile(tilePosition, floorTile);
             }
         }
